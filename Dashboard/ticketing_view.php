4829c7d3950c573d6d02beeb2a42ea2867ad0d32
weimingsoh <weiming.soh.2017@sis.smu.edu.sg>
2019-10-04 13:09:59 +0800

    Adding ticketing system.
    Adding ticketing system.

---
+++ Dashboard/ticketing_view.php
@@ -0,0 +1,43 @@
+<table style="width:100%">
+  <tr>
+    <th>Age</th>
+    <th>Race</th>
+    <th>Gender</th>
+    <th>Description</th>
+    <th>Location</th>
+    <th>Time</th>
+    <th>Date</th>
+    <th>Weather</th>
+    <th>Dangerous</th>
+    <th>Smell?</th>
+    <th>Solved?</th>
+  </tr>
+  <tr>
+    <td>from 40 to 50</td>
+    <td>null</td>
+    <td>Male</td>
+    <td>This old man is wearing a blue shirt with black pants</td>
+    <td>81 Victoria St</td>
+    <td>23:01:25</td>
+    <td>05-10-2019</td>
+    <td>Rainy</td>
+    <td>Yes</td>
+    <td>No</td>
+    <td><a href="ticketing_view2.php" class="button" value ="Solved?">Solved</td>
+
+  </tr>
+  <tr>
+    <td>From 40 to 50</td>
+    <td>null</td>
+    <td>Female</td>
+    <td>This old women is wearing a black shirt with blue pants</td>
+    <td>80 Victoria St</td>
+    <td>18:30:25</td>
+    <td>04-10-2019</td>
+    <td>Rainy</td>
+    <td>Yes</td>
+    <td>No</td>
+    <td><a href="ticketing_view2.php" class="button" value ="Solved?">Solved</td>
+
+  </tr>
+</table>
